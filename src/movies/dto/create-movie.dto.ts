fd088b01b8a3bae679ee584721f8cfcfe464ed6b
Koushal Patil <koushal1000@gmail.com>
2025-09-21 17:39:21 +0530

    Initial Setup for movie entity hand Handling of Poster using s3

---
+++ src/movies/dto/create-movie.dto.ts
@@ -0,0 +1,5 @@
+export class CreateMovieDto {
+  title: string;
+  publishingYear: number;
+  posterUrl: string;
+}
---
+++ src/movies/movie.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  publishingYear: number;
+
+  @Column()
+  posterUrl: string;
+}
